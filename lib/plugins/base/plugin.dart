739d8d5d0338cc9b2b9b27214342d84cdb467c12
dragon <clragon@clynamic.net>
2023-11-08 17:16:01 +0100

    removed logger override

--- lib/plugins/base/plugin.dart
+++ lib/plugins/base/plugin.dart
@@ -2,8 +2,6 @@ import 'package:nyxx/nyxx.dart';
 export 'package:nyxx/nyxx.dart' show NyxxGateway;
 
 abstract class BotPlugin extends NyxxPlugin<NyxxGateway> {
-  late final Logger logger = Logger(name);
-
   String? helpText;
 
   String? buildHelpText(NyxxGateway client) => helpText;
